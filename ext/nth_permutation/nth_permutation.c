344b53836581cd08953c3849a988b39fc43bb5d1
tanvir hasan <tanvir002700@gmail.com>
2018-02-13 19:59:49 +0600

    add test method

--- ext/nth_permutation/nth_permutation.c
+++ ext/nth_permutation/nth_permutation.c
@@ -1,6 +1,12 @@
 #include<ruby.h>
 
+VALUE test()
+{
+    return rb_str_new2("test method");
+}
+
 void Init_nth_permutation()
 {
     VALUE mNthPermutation = rb_define_module("NthPermutation");
+    rb_define_singleton_method(mNthPermutation, "test", test, 0);
 }
