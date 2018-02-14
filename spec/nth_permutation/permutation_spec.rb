27e907527e027283c2fc86c1b739e970d16e55f5
tanvir hasan <tanvir002700@gmail.com>
2018-02-14 20:32:10 +0600

    update spec configuration and basic spec

--- spec/nth_permutation/permutation_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe Nth::Permutation do
-  it "has a version number" do
-    expect(Nth::Permutation::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require "bundler/setup"
-require "nth/permutation"
+require "nth_permutation"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -7,8 +7,10 @@ RSpec.configure do |config|
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
+  config.expose_dsl_globally = true
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
 end
+
