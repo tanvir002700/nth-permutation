fa94943ea74263f1dc342ad00be1db775497e98d
tanvir hasan <tanvir002700@gmail.com>
2018-02-16 13:28:06 +0600

    add frequency calculation function

--- ext/nth_permutation/permutation_string.c
+++ ext/nth_permutation/permutation_string.c
@@ -16,28 +16,32 @@ static void deallocate(struct Str *str)
 static VALUE allocate(VALUE klass)
 {
     struct Str * str = (struct Str *)malloc(sizeof(struct Str));
-
     VALUE obj = Data_Make_Struct(klass, struct Str, NULL, deallocate, str);
-
     str->ptr = NULL;
     str->factorial = NULL;
     str->frequency = NULL;
-
     return obj;
 }
 
 int * factorial()
 {
     long long int * fact = (long long int *) calloc(21, sizeof(long long int));
-
     fact[0] = 1;
-
     for(int i=1; i<21; i++)
     {
         fact[i] = fact[i-1] * i;
     }
-
     return fact;
+}
+
+int * frequency(char * str)
+{
+    int * freq = (int *) calloc(257, sizeof(int));
+    for(int i=0; str[i]; i++)
+    {
+        freq[str[i]]++;
+    }
+    return freq;
 }
 
 static VALUE initialize(VALUE self, VALUE rb_string)
@@ -52,6 +56,9 @@ static VALUE initialize(VALUE self, VALUE rb_string)
     memcpy(str->ptr, StringValuePtr(rb_string), RSTRING_LEN(rb_string));
 
     str->factorial = factorial();
+    str->frequency = frequency(str->ptr);
+
+    for(int i='a'; i<'z'; i++)fprintf(stderr, "%d\n", str->frequency[i]);
 
     rb_iv_set(self, "@str", rb_string);
     rb_iv_set(self, "@length", INT2NUM(RSTRING_LEN(rb_string)));
